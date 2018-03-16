679de39c527479da60bfdc1698368faa3b36e6c2
soblin <hilo.mamoru@gmail.com>
2018-03-16 22:09:09 +0900

    added fibonacci_action (Fri Mar 16 22:09:09 JST 2018)

---
+++ chapter1_codes/fibonacci_action/src/fibonacci_client.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+//#include <actionlib/client/terminal_state.h>
+#include <fibonacci_action/FibonacciAction.h>
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "fibonacci_client_node");
+
+    actionlib::SimpleActionClient<fibonacci_action::FibonacciAction> action_client_("fibonacci_server_node", true);
+
+    // wait for the sepcified server(=fibonacci_server_node).
+    action_client_.waitForServer();
+    fibonacci_action::FibonacciGoal goal;
+    goal.order = 20;
+    action_client_.sendGoal(goal);
+
+    bool finished_before_timeout = action_client_.waitForResult(ros::Duration(30, 0));
+
+    if(finished_before_timeout){
+        actionlib::SimpleClientGoalState state = action_client_.getState();
+        ROS_INFO("Action finished: %s", state.toString().c_str());
+    }
+    else{
+        ROS_INFO("Action did not finish before time out");
+    }
+    return 0;
+}
